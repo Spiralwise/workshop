55507eac0d9c2d4759009a0886e64c20a988f4aa
Spiralwise <sebounart@gmail.com>
2015-08-08 02:17:25 +0200

    Odict release !

--- python/odict.py
+++ python/odict.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3.4
 #-*- coding:utf-8 -*-
 
-class Ordict:
+class Odict:
     """Ordered dictonnary."""
 
     def __init__(self, **words):
         self._keys = []
         self._words = []
-        for key, word in words:
+        for key, word in words.items():
             self._keys.append(key)
             self._words.append(word)
 
@@ -38,13 +38,80 @@ class Ordict:
 
     def __str__(self):
         buffer = "{"
-        for key in self._keys:
+        last_idx = len(self._keys)-1
+        for key, word in zip(self._keys, self._words):
             idx = self._keys.index(key)
-            last_idx = len(self._keys)-1 
-            buffer += key + ": " + self._words[idx]
+            buffer += "'" + key + "': " + str(word)
             if idx != last_idx:
-                buffer += ","
+                buffer += ", "
         buffer += "}"
+        return buffer
 
+    def __repr__(self):
+        return str(self)
+    
     def sort(self):
-        # TO CONTINUE
+        self._words = [w for (k,w) in sorted(zip(self._keys, self._words))]
+        self._keys.sort()
+
+    def reverse(self):
+        self._words = [w for (k,w) in sorted(zip(self._keys, self._words), reverse=True)]
+        self._keys.sort(reverse=True)
+
+    def __iter__(self):
+        self._iter_idx = 0
+        return self
+
+    def __next__(self):
+        if self._iter_idx < len(self._keys)-1:
+            self._iter_idx += 1
+            return (self._keys[self._iter_idx], self._words[self._iter_idx])
+        raise StopIteration
+
+    def keys(self):
+        return self._keys;
+
+    def values(self):
+        return self._words;
+
+    def items(self):
+        return zip(self._keys, self._words)
+
+    def __len__(self):
+        return len(self._keys)
+
+    def __add__(self, other):
+        self._keys += other._keys
+        self._words += other._words
+        return self;
+
+
+if __name__ == "__main__":
+    fruits = Odict()
+    print("Create a new odict: ", fruits)
+    fruits["pomme"] = 52
+    fruits["poire"] = 34
+    fruits["prune"] = 128
+    fruits["melon"] = 15
+    print("Add some fruits: ", fruits)
+    print("Show prune: ", fruits['prune'])
+    fruits.sort()
+    print("Sort: ", fruits)
+    legumes = Odict(carotte = 26, haricot = 48)
+    print("Create a new odict: ", legumes)
+    print("Number of elements: ", len(legumes))
+    legumes.reverse()
+    print("Reverse: ", legumes)
+    fruits = fruits + legumes
+    print("Merge odicts: ", fruits)
+    print("Haricot here?", "haricot" in fruits)
+    del fruits['haricot']
+    print("Del haricot:", "haricot" in fruits)
+    fruits['poire'] = 96;
+    print("Poire=", fruits['poire'])
+    for k in legumes:
+        print(k)
+    print(legumes.keys())
+    print(legumes.values())
+    for n, q in legumes.items():
+        print("{} ({})".format(n, q))
