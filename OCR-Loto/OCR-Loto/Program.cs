c005ee1b770b0fa15453a1f007397a60adac2d8b
Spiralwise <contact@spiralwise.net>
2018-06-26 15:22:24 +0200

    OpenClassRoom Csharp Activity 1

---
+++ OCR-Loto/OCR-Loto/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCR_Loto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> tirages = GenerateTirage();
+            PrintTirage(tirages);
+        }
+
+        static List<int> GenerateTirage()
+        {
+            Random rand = new Random();
+            List<int> tirages = new List<int>();
+
+            int tirage;
+            while (tirages.Count < 7)
+            {
+                tirage = 1 + rand.Next(49);
+                if (tirages.IndexOf(tirage) == -1)
+                    tirages.Add(tirage);
+            }
+            tirages.Sort();
+            return tirages;
+        }
+
+        static void PrintTirage(List<int> l)
+        {
+            foreach (int x in l)
+                Console.WriteLine(x);
+        }
+    }
+}
